6c8865f7e196a7e894d794d9f7c3ea8253c56d2a
Gauravkumar <kumarg2060@gmail.com>
2026-07-06 13:42:48 +0530

    Implement line count analyzer

---
+++ src/analyzer.py
@@ -0,0 +1,6 @@
+def analyze_file(content):
+    line=content.split("\n")
+    count=0;
+    for i in range(0,len(line)):
+        count+=1
+    return count;
--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
 import reader
+import analyzer
 content=reader.read_file()
+total_lines=analyzer.analyze_file(content)
 print("\n-----content of the file-----\n",content)
+print("\n-----total lines in the file-----\n",total_lines)
