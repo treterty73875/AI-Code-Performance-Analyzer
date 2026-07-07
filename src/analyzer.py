4f54d7202af5ee64e94c858f2f9bfe311a9e6842
Gauravkumar <kumarg2060@gmail.com>
2026-07-07 12:43:45 +0530

    improve analysis report formatting

--- src/analyzer.py
+++ src/analyzer.py
@@ -1,6 +1,39 @@
-def analyze_file(content):
-    line=content.split("\n")
+def analyze_content(content):
+    line_count=count_lines(content)
+    word_count=count_words(content)
+    blank_line_count=count_blank_lines(content)
+    character_count=count_characters(content)
+    return line_count, word_count, blank_line_count, character_count
+
+
+def count_lines(content):
+    lines=content.split("\n")
     count=0;
-    for i in range(0,len(line)):
+    for i in range(0,len(lines)):
         count+=1
     return count;
+
+
+def count_words(content):
+    words=content.split()
+    count=0;
+    for i in range(0,len(words)):
+       count+=1
+    return count;
+
+
+def count_blank_lines(content):
+    blank_lines=content.split("\n")
+    count=0;
+    for i in range(0,len(blank_lines)):
+        if blank_lines[i].strip() == "":
+            count+=1
+    return count;
+
+def count_characters(content):
+    count=0;
+    for i in range(0,len(content)):
+        count+=1
+    return count;
+
+
--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
 import reader
 import analyzer
 content=reader.read_file()
-total_lines=analyzer.analyze_file(content)
+line_count, word_count, blank_line_count, character_count=analyzer.analyze_content(content)
 print("\n-----content of the file-----\n",content)
-print("\n-----total lines in the file-----\n",total_lines)
+print("\n--------Analysis report--------\n",)
+print("Total number of lines:", line_count)
+print("Total number of words:", word_count)
+print("Total number of blank lines:", blank_line_count)
+print("Total number of characters:", character_count)
+print("\n-------------------------------\n")
+
