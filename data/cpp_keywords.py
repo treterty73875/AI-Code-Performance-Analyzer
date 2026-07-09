a8a936ea5d84a74a64da1993a17adc2fe4d9b242
Gauravkumar <kumarg2060@gmail.com>
2026-07-09 10:19:00 +0530

    List of C++ keywords

---
+++ data/cpp_keywords.py
@@ -0,0 +1,15 @@
+cpp_keywords ={"for":"loop",
+               "while":"loop",
+               "do":"loop",
+
+               "if":"conditional",
+               "else":"conditional",
+               "switch":"conditional",
+
+               "int":"datatype",
+               "float":"datatype",
+               "double":"datatype",
+               "char":"datatype",
+               
+               "return":"keyword"
+               }
